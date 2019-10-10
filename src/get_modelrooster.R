18a76e89e2c77c66f579a129496580a89dbd9571
thelonius182 <lcavdakker@gmail.com>
2019-10-10 23:15:14 +0200

    again

--- src/get_modelrooster.R
+++ src/get_modelrooster.R
@@ -5,3 +5,4 @@ library(keyring)
 drive_auth(path = key_get(service = "cz-sheets-processing", username = "access-token"))
 drive_find("cup")
 drive_download(file = as_id("10ZlUGqnG_yWGAgVYx7k6XD_i5BD9K2OUi2NJXu0E_Gc"))
+# maar het gaat nog verder
