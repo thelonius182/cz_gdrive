ab8b90547d33850eb532f59c5360ff7a27206f93
thelonius182 <lcavdakker@gmail.com>
2019-10-13 01:46:11 +0200

    rename & simplify

--- src/get_google_czdata.R
+++ src/get_google_czdata.R
@@ -8,6 +8,11 @@ cz_extract_sheet <- function(ss_name, sheet_name) {
             .name_repair = ~ ifelse(nzchar(.x), .x, LETTERS[seq_along(.x)]))
 }
 
+cz_get_url <- function(cz_ss) {
+  cz_url <- paste0("url_", cz_ss)
+  paste0("https://", config$url_pfx, config[[cz_url]])
+}
+
 config <- read_yaml("config.yaml")
 
 # downloads GD ------------------------------------------------------------
@@ -16,22 +21,22 @@ config <- read_yaml("config.yaml")
 drive_auth(path = key_get(service = "cz-sheets-processing", username = "access-token"))
 
 # Roosters 3.0 ophalen bij GD
-path_rooster_30 <- paste0(config$gs_downloads, "/", "roosters_30.xlsx")
-drive_download(file = config$url_roosters, overwrite = T, path = path_rooster_30)
+path_roosters <- paste0(config$gs_downloads, "/", "roosters.xlsx")
+drive_download(file = cz_get_url("roosters"), overwrite = T, path = path_roosters)
 
 # iTunes-cupboard ophalen bij GD
 path_itunes_cupboard <- paste0(config$gs_downloads, "/", "itunes_cupboard.xlsx")
-drive_download(file = config$url_itunes_cupboard, overwrite = T, path = path_itunes_cupboard)
+drive_download(file = cz_get_url("itunes_cupboard"), overwrite = T, path = path_itunes_cupboard)
 
 # WP-gids-info ophalen bij GD
 path_wp_gidsinfo <- paste0(config$gs_downloads, "/", "wordpress_gidsinfo.xlsx")
-drive_download(file = config$url_wordpress_gidsinfo, overwrite = T, path = path_wp_gidsinfo)
+drive_download(file = cz_get_url("wordpress_gidsinfo"), overwrite = T, path = path_wp_gidsinfo)
 
 # sheets als df -----------------------------------------------------------
 
-tbl_presentatie <- cz_extract_sheet(path_rooster_30, sheet_name = "presentatie")
-tbl_montage <- cz_extract_sheet(path_rooster_30, sheet_name = "montage")
-tbl_zenderschema <- cz_extract_sheet(path_rooster_30, sheet_name = paste0("modelrooster-", config$modelrooster_versie))
+tbl_presentatie <- cz_extract_sheet(path_roosters, sheet_name = "presentatie")
+tbl_montage <- cz_extract_sheet(path_roosters, sheet_name = "montage")
+tbl_zenderschema <- cz_extract_sheet(path_roosters, sheet_name = paste0("modelrooster-", config$modelrooster_versie))
 tbl_itunes_cupboard <- cz_extract_sheet(path_itunes_cupboard, sheet_name = "playlist_names")
 tbl_gidsinfo <- cz_extract_sheet(path_wp_gidsinfo, sheet_name = "gids-info")
 tbl_gidsinfo_nl_en <- cz_extract_sheet(path_wp_gidsinfo, sheet_name = "vertalingen NL-EN")
